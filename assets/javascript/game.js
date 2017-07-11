6d1124dfc8b008ef20cfe0fa07e1800286cb8a80
Cristina Salvo <csalvo@spendsetter.com>
2017-07-10 21:38:45 -0400

    adding a score and guess counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,27 @@
 var words = ["Albarino", "Barbera", "Blush", "Bordeaux", "Cabernet Franc", "Cabernet Sauvignon", "Carmenere", "Chambourcin", "Champagne", "Chardonnay", "Chenin Blanc", "Chianti", "Eiswein", "Gewurztraminer", "Grappa", "Grenache", "Lambrusco", "Malbec",  "Marsala", "Meritage", "Merlot","Moscato", "Muscat","Petit Verdot", "Petite Sirah", "Pinot Blanc", "Pinot Grigio", "Pinot Noir", "Pinotage", "Prosecco", "Sangiovese", "Sauvignon Blanc", "Schiava", "Shiraz", "Semillon", "Sherry", "Soave", "Tannat", "Tempranillo", "Torrontes", "Trebbiano", "Valpolicella", "Verdejo", "Verdicchio", "Vinho Verde", "Viognier", "Zinfandel", "grapes", "blend", "varietals", "barrels", "bottle", "cellar", "vineyard", "California", "France", "Italy", "Spain", "Argentina", "Australia", "corkscrew", "sangria", "tannins", "vermouth", "vintage", "sommelier", "tasting"];
 var hiddenWord;
 var guesses;
-var score;
+var score = 0;
 var chosenWord;
 var hiddenArray
+var usedLetters;
 
 //how the game starts - reset guesses, choose a random word, present its hashed value to the user
 function gameStart() {
-	guesses = 20;
+	usedLetters = [];
+	guesses = 12;
+	document.getElementById("guessed").innerHTML = usedLetters;
 	var wordIndex = Math.floor(Math.random() * words.length + 1);
 	chosenWord = words[wordIndex].toLowerCase().split("");
 	var hideWord = "_ ";
 	hiddenWord = hideWord.repeat(chosenWord.length).split(" ");
-	hiddenArray = hiddenWord;
 	document.getElementById("hiddenWord").innerHTML = hiddenWord.join(" ");
 }
 
-
+//executes the main plays and rules of the game
 function gamePlay() {
 	var letterIndex = [];
-	var usedLetters = [];
+
 
 	//when a user inputs a letter, find all of its occurrences 
 	document.onkeyup = function(event){
@@ -27,46 +29,54 @@ function gamePlay() {
 		//clear letter indexes for next guess	
 		letterIndex = [];
 
-
+//if the guessed letter is not in the word, and there are remaining guesses, display in used letter array
 		if (chosenWord.indexOf(event.key) === -1 && (event.keyCode >= 65 && event.keyCode <=90)) {
-			usedLetters.push(event.key.toUpperCase());
-			console.log(usedLetters);
-        	document.getElementById("guessed").innerHTML = usedLetters.join(", ");
+				// if there are no guesses left, end the game 
+				if (guesses <= 0) {
+					alert("You lose!");
+        			document.getElementById("hiddenWord").innerHTML = chosenWord.join(" ").toUpperCase();
+        			gameStart();
+					gamePlay();
 
+				}
+				//if there are, add them to the array
+				else if (guesses > 0) {
+					usedLetters.push(event.key.toUpperCase());
+        			document.getElementById("guessed").innerHTML = usedLetters.join(", ");
+        			guesses--
+        			document.getElementById("guesses-remaining").innerHTML = guesses;
+        		}
 		}
-
+//if the letter is in the word, find the indexes of everywhere it appears in the word, then place those into the word	
+		//find indexes
 		else if (chosenWord.indexOf(event.key >= 0)){
 			for (var i = 0; i < chosenWord.length; i++) {
 					if (chosenWord[i] === event.key) {
 					letterIndex.push(i);
-					console.log(letterIndex);
 				}
 			}
 		}
-
+		//replace the dash with the letter where it appears in the word
 		for (var i = 0; i < letterIndex.length; i++) {
 			hiddenWord[letterIndex[i]] = event.key.toUpperCase();
 			document.getElementById("hiddenWord").innerHTML = hiddenWord.join(" ");
- 
-		}
+			}
 
-	}	
+			if (hiddenWord.indexOf("_") === -1 && usedLetters.length < 12){
+			document.getElementById("hiddenWord").innerHTML = hiddenWord.join(" ");
+
+			score++;
+			document.getElementById("score").innerHTML = score;
+			
+			gameStart();
+			gamePlay();
+
+		} 
+	}
 }
+			gameStart();
+			gamePlay();
 
-
-
-
-		gameStart();
-		gamePlay();
-
-
-
-
-
-// function gamePlay()
-// each time a letter is pressed, if it matches a letter in hidden word, display that letter at the right index in the word
-// if it does not match, add it to alreadyGuessed
-// decrement guesses for both of these 
-// do this until all of the letters have been guessed, or until guesses = 0
-// if there are no more letters to be guessed increment score by 1
-// if guesses = 0 or there are no more letters in the word to guess, call game start()
+//press any key to start
+//fix bug where the last letter doesn't display before ending thegame
+//
